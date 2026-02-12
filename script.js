d7efb14bd921c80d56ed433b43c15abc63e532ca
jidel abdelkader <djidelabdocs50@gmail.com>
2026-02-12 15:37:29 +0200

    Add typing effect for developer description

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio Loaded Successfully");
+const text = "Web Developer & Cyber Security Enthusiast";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 80);
+    }
+}
+
+window.onload = typeEffect;
+
+
